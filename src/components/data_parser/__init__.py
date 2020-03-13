7d6dbbee0bc908cf25d70b4b9656a301062b047a
Jordi Armengol Estapé <me@jordiae.com>
2020-03-13 16:31:47 +0100

    Added Wikipedia and BSC crawler parsers

--- src/components/data_parser/__init__.py
+++ src/components/data_parser/__init__.py
@@ -1,6 +1,6 @@
 from .data_parser import DataParser
 from .wikipedia_parser import WikipediaParser
 from .bsc_crawl_json_parser import BSCCrawlJSONParser
-from .data_parser_factory import DataParserFactory
+from .data_parser_factory import data_parser_factory
 
-__all__ = ['DataParser', 'WikipediaParser', 'BSCCrawlJSONParser', 'DataParserFactory']
+__all__ = ['DataParser', 'WikipediaParser', 'BSCCrawlJSONParser', 'data_parser_factory']
--- src/components/data_parser/data_parser_factory.py
+++ src/components/data_parser/data_parser_factory.py
@@ -2,9 +2,8 @@ from .wikipedia_parser import  WikipediaParser
 from .bsc_crawl_json_parser import BSCCrawlJSONParser
 
 
-class DataParserFactory:
-    def __init__(self, path: str, parser_type: str):
-        if parser_type == 'wikipedia':
-            return WikipediaParser
-        elif parser_type == 'bsc-crawl-json':
-            return BSCCrawlJSONParser
+def data_parser_factory(path: str, parser_type: str):
+    if parser_type == 'wikipedia':
+        return WikipediaParser(path)
+    elif parser_type == 'bsc-crawl-json':
+        return BSCCrawlJSONParser(path)
