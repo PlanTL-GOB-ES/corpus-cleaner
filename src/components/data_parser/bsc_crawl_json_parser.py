a51050d33d7135afefd8388ca937e7d2141736b3
Jordi Armengol Estapé <me@jordiae.com>
2020-03-16 17:14:59 +0100

    Refactored parser, added encoding checking.

--- src/components/data_parser/bsc_crawl_json_parser.py
+++ src/components/data_parser/bsc_crawl_json_parser.py
@@ -1,27 +1,28 @@
 from components.data_parser import DataParser
 from typing import Iterable
 from document import Document
-import os
 import json
+from typing import TextIO
 
 
 class BSCCrawlJSONParser(DataParser):
-    def parse(self) -> Iterable[Document]:
-        i = 1
-        for chunk in sorted(os.listdir(self.path)):
-            for filename in sorted(os.listdir(os.path.join(self.path, chunk))):
-                with open(os.path.join(self.path, chunk, filename), 'r', errors='ignore') as f:
-                    for idx, line in enumerate(f.readlines()):
-                        i += idx
-                        j = json.loads(line)
-                        url = j['url']
-                        keywords = j['url']
-                        content = j['p']
-                        heads = j['heads']
-                        title = j['titles']
-                        filename = os.path.join(chunk, filename)
-                        yield Document(content=content, filename=filename, url=url, id_=i,
-                                       keywords=keywords, heads=heads, title=title)
+
+    def __init__(self, *args, **kwargs):
+        super(BSCCrawlJSONParser, self).__init__(*args, extensions=['*.json'], **kwargs)
+
+    def _parse_file(self, fd: TextIO, relative_filepath: str, doc_counter: int) -> Iterable[Document]:
+        i = doc_counter + 1
+        for idx, line in enumerate(fd.readlines()):
+            j = json.loads(line)
+            url = j['url']
+            keywords = j['url']
+            content = j['p']
+            heads = j['heads']
+            title = j['titles']
+            filename = relative_filepath
+            yield Document(content=content, filename=filename, url=url, id_=i,
+                           keywords=keywords, heads=heads, title=title)
+            i += idx
 
 
 def test():
@@ -29,7 +30,7 @@ def test():
     parser = BSCCrawlJSONParser(file_dir)
     documents = parser.parse()
 
-    # Show the first two documents
+    # Show the first document
     for idx, doc in enumerate(documents):
         print(f'DOC {idx}: {doc.content}\n')
         if idx == 1:
--- src/components/data_parser/data_parser.py
+++ src/components/data_parser/data_parser.py
@@ -1,10 +1,54 @@
 from typing import Iterable
 from document import Document
+from chardet.universaldetector import UniversalDetector  # TODO: Try UnicodeDammit, Magic...?
+from typing import TextIO
+import os
+from typing import List
+from pathlib import Path
 
 
 class DataParser:
-    def __init__(self, path: str):
+    def __init__(self, path: str, extensions: List[str], encoding: str = 'auto', encoding_threshold: float = 0.9,
+                 error_policy: str = 'ignore'):
         self.path = path
+        self.extensions = extensions
+        self.encoding = encoding
+        self.encoding_threshold = encoding_threshold  # TODO: Revisit default*
+        self.error_policy = error_policy  # TODO: Revisit default
 
     def parse(self) -> Iterable[Document]:
+        doc_counter = 0
+        for idx_filepath, relative_filepath in enumerate(sorted(self._get_relative_filepaths())):
+            abs_path = os.path.join(self.path, relative_filepath)
+            enc = self._guess_encoding(abs_path) if self.encoding == 'auto' else self.encoding
+            with open(abs_path, 'r', encoding=enc, errors='ignore') as f:
+                for doc in self._parse_file(f, relative_filepath, doc_counter):
+                    if enc != 'utf-8':
+                        pass  # TODO: Check possible problems when the original file was not utf-8
+                    yield doc
+                    doc_counter += 1
+
+    def _parse_file(self, fd: TextIO, relative_filepath: str, doc_counter: int) -> Iterable[Document]:
         raise NotImplementedError()
+
+    def _get_relative_filepaths(self) -> Iterable[str]:
+        relative_paths = []
+        for extension in self.extensions:
+            for path in Path(self.path).rglob(extension):
+                if os.path.isfile(path):
+                    relative_paths.append(os.path.join(os.path.relpath(path.parents[0], self.path), path.name))
+        return relative_paths
+
+    def _guess_encoding(self, path: str):
+        # https://stackoverflow.com/questions/46037058/using-chardet-to-find-encoding-of-very-large-file/49621821
+        detector = UniversalDetector()
+        detector.reset()
+        with open(path, 'rb') as f:
+            for row in f:
+                detector.feed(row)
+                if detector.done:
+                    break
+        detector.close()
+        encoding = detector.result['encoding'] if detector.result['confidence'] > self.encoding_threshold else 'utf-8'
+        return encoding
+
