39be0935804f2bf42755a71e80595545c6243772
antonio miranda <antonio.miranda@alumnos.uc3m.es>
2020-03-17 15:49:35 +0100

    Added heur_filters function to PreFilterer class

--- src/components/pre_filterer/pre_filterer.py
+++ src/components/pre_filterer/pre_filterer.py
@@ -2,6 +2,8 @@ from typing import Union, Set
 from typing import Iterable
 from document import Document
 import re
+from langid.langid import LanguageIdentifier, model # fast
+
 
 
 class PreFilterer:
@@ -55,3 +57,61 @@ class PreFilterer:
                     break
             if keep:
                 yield doc
+                
+                
+    def heur_filters(self, text, thres_length=40, thres_digit=0.1, thres_alpha=0.05, 
+                     thres_upper=0.1, target_lang='es', thres_conf=0.95):
+        '''
+        Remove documents: non-Spanish
+                          short length
+                          high a ratio: 
+                                    digits,  
+                                    uppercase,
+                                    non-alphanumeric
+        Parameters
+        ----------
+        text : list of strings
+            Every element in the text is a string containing one single sentence
+        thres_length: int
+            Minimum document length to keep it.
+        thres_digit: float
+            Maximum proportion of digits in document to keep it
+        thres_alpha: float
+            Maximum proportion of alphanumeric characters in document to keep it
+        thres_upper: float
+            Maximum proportion of uppercase characters in document to keep it
+        target_lang: str
+            Language code we want to keep (es, cat, eu, etc)
+        thres_conf: float
+            Minimum language confidence to keep it
+            
+        Returns
+        -------
+        bool
+            Whether to keep document or not
+        '''
+             
+        ## Length condition
+        l = len(text)
+        if (l < thres_length):
+            return False
+        
+        ## Ratio conditions
+        # Digit ratio filter
+        if sum(c.isdigit() for c in text)/l > thres_digit:
+            return False
+        # Not alphanumeric filter
+        if (1 - (sum(c.isalnum() for c in text)/l)) > thres_alpha:
+            return False
+        # Uppercase ratio filter
+        if sum(c.isupper() for c in text)/l > thres_upper:
+            return False
+        
+        ## Language condition
+        identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+        my_model = identifier.classify(text)
+        if ((my_model[0] == target_lang) & 
+            (my_model[1]>thres_conf)):
+            return True
+        else:
+            return False
