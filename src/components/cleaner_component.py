e07c1b34e217aaf00d9787bbe802dff2a181822e
Jordi Armengol Estapé <me@jordiae.com>
2020-03-20 18:12:57 +0100

    Fixed exception

--- src/components/cleaner_component.py
+++ src/components/cleaner_component.py
@@ -4,4 +4,4 @@ import argparse
 class CleanerComponent:
     @staticmethod
     def add_args(parser: argparse.ArgumentParser):
-        raise NotImplementedError
+        raise NotImplementedError()
