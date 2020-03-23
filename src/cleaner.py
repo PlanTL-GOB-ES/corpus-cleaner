aff493740909e036a97cd146c5df2ca4670c2c28
Jordi Armengol Estapé <me@jordiae.com>
2020-03-23 16:17:51 +0100

    Cleaning script working, except for the Cleaner class

--- src/cleaner.py
+++ src/cleaner.py
@@ -6,14 +6,14 @@ from components.data_parser.data_parser import DataParser
 from components.pre_filterer.pre_filterer import PreFilterer
 from components.encoding_fixer.encoding_fixer import EncodingFixer
 from components.normalizer.normalizer import Normalizer
-from components.sentence_splitter._sentence_splitter import _SentenceSplitter
+from components.sentence_splitter_component.sentence_splitter_component import SentenceSplitterComponent
 from components.sentence_filter.sentence_filter import SentenceFilter
 from components.document_filter.document_filter import DocumentFilter
 from components.document_organizer.document_organizer import DocumentOrganizer
 from .components.output_formatter.output_formatter import OutputFormatter
 
 COMPONENTS_DEFAULT = [DataParser, PreFilterer, EncodingFixer,
-                      _SentenceSplitter, SentenceFilter, Normalizer,
+                      SentenceSplitterComponent, SentenceFilter, Normalizer,
                       DocumentFilter, DocumentOrganizer, OutputFormatter]
 
 
