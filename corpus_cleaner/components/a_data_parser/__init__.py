2354e39c1d9e51ebd8608c5edd5662c3c7a5118f
casimiro pio carrino <c.casimiro88@gmail.com>
2020-05-05 17:25:56 +0200

    * add OnionParser class * update requirements list

--- corpus_cleaner/components/a_data_parser/__init__.py
+++ corpus_cleaner/components/a_data_parser/__init__.py
@@ -2,5 +2,6 @@ from .data_parser import DataParser
 from .wikipedia_parser import WikipediaParser
 from .bsc_crawl_json_parser import BSCCrawlJSONParser
 from .data_parser_factory import DataParserFactory
+from .onion_parser import OnionParser
 
-__all__ = ['DataParser', 'WikipediaParser', 'BSCCrawlJSONParser', 'DataParserFactory']
+__all__ = ['DataParser', 'WikipediaParser', 'BSCCrawlJSONParser', 'DataParserFactory', 'OnionParser']
