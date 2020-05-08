c24b90498adc5f98e9a86b93283696a21dfb27a0
Jordi Armengol Estapé <me@jordiae.com>
2020-05-08 13:26:17 +0200

    Fixed Docker image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:18.04
 
 RUN  apt-get update \
+  && apt-get install -y apt-utils \
   && apt-get install -y wget \
   && apt-get install -y git \
   && apt-get install -y python3 \
--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
 docker build -t corpuscleaner . && docker run -v output:/output -v data:/data -it corpuscleaner
-
-docker build -t dv . && docker run -v mount:/mount -it dv $*
