09f66e7fb5f71f1ec748cee9c4e82d3c493c4fcf
Jordi Armengol Estapé <me@jordiae.com>
2020-05-11 12:57:21 +0200

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf data/ & rm -rf output/ & ln -s /data CorpusCleaner/data & ln -s /output CorpusCleaner/output & python3 CorpusCleaner/clean.py $*
+ls CorpusCleaner/data && rm -rf data/ && rm -rf output/ && ln -s /data CorpusCleaner/data && ln -s /output CorpusCleaner/output && python3 CorpusCleaner/clean.py $*
