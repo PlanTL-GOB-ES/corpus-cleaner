f598b6e89d1cddb914bb64e48076bb651b94c5f8
Jordi Armengol Estapé <me@jordiae.com>
2020-05-11 12:05:23 +0200

    Fixed docker install

--- get-third-party-docker.sh
+++ get-third-party-docker.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
-cd CorpusCleaner
-
-mkdir -p lib
+mkdir -p CorpusCleaner/lib
 
 # Onion
 wget -O onion-1.2.tar.gz 'http://corpus.tools/raw-attachment/wiki/Downloads/onion-1.2.tar.gz'
 tar xzf onion-1.2.tar.gz
-mv onion-1.2 lib
-cd lib/onion-1.2/
-PREFIX=$(pwd)
+mv onion-1.2 CorpusCleaner/lib
+PREFIX=CorpusCleaner/lib/onion-1.2/
 echo "#JUDY_INC=-I/opt/local/include
 #JUDY_LIB=-L/opt/local/lib
 PREFIX=${PREFIX}
 INSTALL_BIN=${PREFIX}/bin
-INSTALL_DATA=${PREFIX}/share" > Makefile.config
-make
-make install
+INSTALL_DATA=${PREFIX}/share" > CorpusCleaner/lib/onion-1.2/Makefile.config
+make -C CorpusCleaner/lib/onion-1.2/
+make -C CorpusCleaner/lib/onion-1.2/ install
 
 # fasttext
 wget -q https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
-mv lid.176.bin lib/
+mv lid.176.bin CorpusCleaner/lib/
