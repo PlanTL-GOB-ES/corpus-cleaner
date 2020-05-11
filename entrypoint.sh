906d419545e221b165872de8d1422aa698bb6864
Jordi Armengol Estapé <me@jordiae.com>
2020-05-11 12:35:29 +0200

    Added CLI arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ln -s /data CorpusCleaner/data & ln -s /output CorpusCleaner/output & python3 CorpusCleaner/clean.py $*
+rm -rf data/ & rm -rf output/ & ln -s /data CorpusCleaner/data & ln -s /output CorpusCleaner/output & python3 CorpusCleaner/clean.py $*
