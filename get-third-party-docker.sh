3f1bfce156aabc3d3b1449c69deb94e5a2f1cee2
Jordi Armengol Estapé <me@jordiae.com>
2020-05-11 13:59:30 +0200

    Fixed Onion path in Docker

--- get-third-party-docker.sh
+++ get-third-party-docker.sh
@@ -6,12 +6,11 @@ mkdir -p CorpusCleaner/lib
 wget -O onion-1.2.tar.gz 'http://corpus.tools/raw-attachment/wiki/Downloads/onion-1.2.tar.gz'
 tar xzf onion-1.2.tar.gz
 mv onion-1.2 CorpusCleaner/lib
-PREFIX=lib/onion-1.2
+#PREFIX=lib/onion-1.2
 echo "#JUDY_INC=-I/opt/local/include
 #JUDY_LIB=-L/opt/local/lib
-PREFIX=${PREFIX}
-INSTALL_BIN=${PREFIX}/bin
-INSTALL_DATA=${PREFIX}/share" > CorpusCleaner/lib/onion-1.2/Makefile.config
+INSTALL_BIN=bin
+INSTALL_DATA=share" > CorpusCleaner/lib/onion-1.2/Makefile.config
 make -C CorpusCleaner/lib/onion-1.2
 make -C CorpusCleaner/lib/onion-1.2 install
 
