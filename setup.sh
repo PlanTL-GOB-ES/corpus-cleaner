57df5d93e297a3f3e17409832c3041ecd8fa70e3
Jordi Armengol Estapé <me@jordiae.com>
2020-05-15 12:34:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Create virtualenv
 ENV_DIR=${SCRIPT_DIR}/venv
-python3.7 -m venv ${ENV_DIR}
+python3 -m venv ${ENV_DIR}
 source ${ENV_DIR}/bin/activate
 
 # Install python dependencies
