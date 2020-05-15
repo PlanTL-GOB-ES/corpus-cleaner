6f0d74acf167cad9e1a1bd8053bf4438364f45ef
Jordi Armengol Estapé <me@jordiae.com>
2020-05-15 12:28:40 +0200

    Named image

--- build-singularity.sh
+++ build-singularity.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $(realpath .):/output  --privileged -t --rm  quay.io/singularity/docker2singularity -m "/scratch/data /scratch/output" corpuscleaner
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $(realpath .):/output  --privileged -t --rm  quay.io/singularity/docker2singularity -m "/scratch/data /scratch/output" --name corpuscleaner-singularity corpuscleaner
--- run-singularity.sh
+++ run-singularity.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo singularity run --writable-tmpfs --bind $(realpath data):/scratch/data --bind $(realpath output):/scratch/output *.sif $*
+sudo singularity run --writable-tmpfs --bind $(realpath data):/scratch/data --bind $(realpath output):/scratch/output corpuscleaner-singularity.sif $*
