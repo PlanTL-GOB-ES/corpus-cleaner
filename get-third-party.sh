945ec2dd1d9646ba81de71a47a7c3d2cf8984a5c
Jordi Armengol Estapé <me@jordiae.com>
2020-05-20 15:09:10 +0200

    Fixed get-third-party.sh

--- get-third-party.sh
+++ get-third-party.sh
@@ -15,6 +15,7 @@ INSTALL_BIN=${PREFIX}/bin
 INSTALL_DATA=${PREFIX}/share" > Makefile.config
 make
 make install
+cd ..
 
 # fasttext
 wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
