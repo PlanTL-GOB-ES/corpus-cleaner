3edc13f7dc04a5b475f96799d1066029e1a30cd5
onadegibert <onadegibert@gmail.com>
2020-06-04 13:17:25 +0200

    Added support for Georgian

--- corpus_cleaner/configs/langs/__init__.py
+++ corpus_cleaner/configs/langs/__init__.py
@@ -4,9 +4,10 @@ from .en import en
 from .eu import eu
 from .es import es
 from .fi import fi
+from .ka import ka
 from .lv import lv
 from .no import no
 
-langs = dict(ca=ca, de=de, en=en, eu=eu, es=es, fi=fi, lv=lv, no=no)
+langs = dict(ca=ca, de=de, en=en, eu=eu, es=es, fi=fi, ka=ka, lv=lv, no=no)
 
 __all__ = ['langs']
--- corpus_cleaner/configs/langs/en.py
+++ corpus_cleaner/configs/langs/en.py
@@ -1,6 +1,6 @@
 import string
 
-alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase)
+alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase))
 fasttext = True
 langid = True
 
