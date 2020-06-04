6d68d830afb61f68fa65600e210e13ad0e91f695
onadegibert <onadegibert@gmail.com>
2020-06-04 11:06:55 +0200

    Added support for Norwegian

--- corpus_cleaner/configs/langs/__init__.py
+++ corpus_cleaner/configs/langs/__init__.py
@@ -5,7 +5,8 @@ from .eu import eu
 from .es import es
 from .fi import fi
 from .lv import lv
+from .no import no
 
-langs = dict(ca=ca, de=de, en=en, eu=eu, es=es, fi=fi, lv=lv)
+langs = dict(ca=ca, de=de, en=en, eu=eu, es=es, fi=fi, lv=lv, no=no)
 
 __all__ = ['langs']
