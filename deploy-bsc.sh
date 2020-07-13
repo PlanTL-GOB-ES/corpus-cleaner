43f4cc753577fd4998cfb31c8e0cba8de42afd49
Jordi Armengol Estapé <me@jordiae.com>
2020-07-13 16:06:26 +0200

    Update deploy-bsc.sh

--- deploy-bsc.sh
+++ deploy-bsc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-USER="bsc88251"
-GROUP="bsc88"
+USER="dummyuser"
+GROUP="dummygroup"
 VERSION=$(date +"%d-%m-%Y")
 DIR_PATH="/gpfs/projects/${GROUP}/corpus-cleaner/${VERSION}"
 ssh ${USER}@dt01.bsc.es 'mkdir -p ' ${DIR_PATH}/data ${DIR_PATH}/output
