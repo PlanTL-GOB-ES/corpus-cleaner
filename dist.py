d43f07e7862db1a35ae32d322ee733e8d2ac1959
Jordi Armengol Estapé <me@jordiae.com>
2020-07-24 19:13:07 +0200

    Fixed YAML template

--- dist.py
+++ dist.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     slaves = hosts[1:]
     timestamp = time.strftime("%Y-%m-%d-%H%M")
     yaml_path = os.path.join('output', f'config-{timestamp}.yaml')
-    singularity = f'cd {os.getcwd()}; module load singularity/3.5.2;' \
-                  f'singularity exec --writable-tmpfs --bind $(realpath data):/cc/data --bind' \
+    singularity = f'cd {os.getcwd()}; module load singularity/3.5.2; ' \
+                  f'singularity exec --writable-tmpfs --bind $(realpath data):/cc/data --bind ' \
                   f'$(realpath output):/cc/output corpuscleaner-singularity.sif bash -c "cd /cc/corpus-cleaner && '
 
     yaml = get_yaml_template(nodes=len(hosts), user=subprocess.getoutput('whoami'), master=master, slaves=slaves,
