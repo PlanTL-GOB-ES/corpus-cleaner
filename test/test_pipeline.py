efc282cbe527aa7b6e3aa2142b5db8b2da89aad2
onadegibert <onadegibert@gmail.com>
2020-07-24 12:24:40 +0200

    Removed duplicated info

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -6,11 +6,8 @@ import re
 date_time = re.sub(r'[T]', '-', datetime.datetime.now().replace(microsecond=0).isoformat()).replace(":", "")[:-2]
 filename = "output/pre-push-test-" + date_time + "/output.txt"
 
-# This function will check if an output file is created and is not empty
 def test_pipeline_execution():
     os.system("python clean.py pre-push-test --input-path test/toy_data --input-format warc --output-format fairseq-lm --lang-filter ca")
-    date_time = re.sub(r'[T]', '-', datetime.datetime.now().replace(microsecond=0).isoformat()).replace(":","")[:-2]
-    filename = "output/pre-push-test-"+date_time+"/output.txt"
     assert os.path.isfile(filename) == True, "Execution unsucessful"
 
 def test_file_exists():
