b2161f81598b46afda346ed6e5f424f77ccfcdeb
Jordi Armengol Estapé <me@jordiae.com>
2020-07-28 02:42:16 +0200

    Update dist.py

--- dist.py
+++ dist.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     timestamp = time.strftime("%Y-%m-%d-%H%M")
     yaml_path = os.path.join('output', f'config-{timestamp}.yaml')
     singularity = f'cd {work_dir}; module load singularity/3.5.2; ' \
-                  f'singularity shell --writable-tmpfs --bind $(realpath data):/cc/data --bind ' \
+                  f'singularity exec --writable-tmpfs --bind $(realpath data):/cc/data --bind ' \
                   f'$(realpath output):/cc/output corpuscleaner-singularity.sif bash -c "cd /cc/corpus-cleaner && '
 
     yaml = get_yaml_template(nodes=len(hosts), user=subprocess.getoutput('whoami'), master=master, slaves=slaves,
