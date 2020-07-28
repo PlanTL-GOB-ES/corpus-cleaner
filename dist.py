7fb183cb9d5bc2134270910d94c2c9f68d6a6d46
Jordi Armengol Estapé <me@jordiae.com>
2020-07-28 23:28:43 +0200

    Fixed YAML

--- dist.py
+++ dist.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     timestamp = time.strftime("%Y-%m-%d-%H%M")
     yaml_path = os.path.join('output', f'config-{timestamp}.yaml')
     singularity1 = f'cd {work_dir}; module load singularity/3.5.2; ' \
-                  f'singularity instance start --writable-tmpfs --bind $(realpath data):/cc/data --bind $(realpath output):/cc/output corpuscleaner-singularity.sif cc' \
-                  f'singularity exec instance://cc bash -c "cd /cc/corpus-cleaner && '
+                   f'singularity instance start --writable-tmpfs --bind $(realpath data):/cc/data --bind $(realpath output):/cc/output corpuscleaner-singularity.sif cc; ' \
+                   f'singularity exec instance://cc bash -c "cd /cc/corpus-cleaner && '
     singularity2 = f'cd {work_dir}; module load singularity/3.5.2; ' \
-                  f'singularity exec instance://cc bash -c "cd /cc/corpus-cleaner && '
+                   f'singularity exec instance://cc bash -c "cd /cc/corpus-cleaner && '
     yaml = get_yaml_template(nodes=len(hosts), user=subprocess.getoutput('whoami'), master=master, slaves=slaves,
                              singularity1=singularity1, singularity2=singularity2)
     # singularity instance start corpuscleaner-singularity.sif prova --writable-tmpfs --bind $(realpath data):/cc/data --bind $(realpath output):/cc/output
