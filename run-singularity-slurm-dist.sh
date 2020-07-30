19ba8a5d1cfb7b99cdc5460afd1a3f502cfbd5bd
Jordi Armengol Estapé <me@jordiae.com>
2020-07-30 17:00:47 +0200

    Distributed FINALLY working

--- run-singularity-slurm-dist.sh
+++ run-singularity-slurm-dist.sh
@@ -2,12 +2,11 @@
 #SBATCH --job-name=corpuscleaner
 #SBATCH --output=logs/corpuscleaner_%j.out
 #SBATCH --error=logs/corpuscleaner_%j.err
-#SBATCH --ntasks=4
+#SBATCH --ntasks=3
 #SBATCH --cpus-per-task=48
 #SBATCH --time=2-00:00:00
 #SBATCH --wait
 #SBATCH --wait-all-nodes=1
-
 
 PARAMETERS="example-output --input-path data/toy_wiki --input-format wikipedia --output-format fairseq-lm --parallel --backend ray --lang-filter ca"
 
@@ -31,10 +30,10 @@ do
   j=$(($i + 1))
   host=$(echo "${hostlist}" | sed "${j}q;d")
   echo $master  ${SLURM_JOB_NUM_NODES} ${i}
-  ssh -n "$host" "module load singularity/3.5.2; cd ${work_dir}; singularity instance start --writable-tmpfs --bind $(realpath data):/cc/data --bind $(realpath output):/cc/output corpuscleaner-singularity.sif cc; ray start --address=<address>; singularity exec instance://cc bash -c \"ray start --address=${master}\"" &
+  ssh -n "$host" "module load singularity/3.5.2; cd ${work_dir}; singularity instance start --writable-tmpfs --bind $(realpath data):/cc/data --bind $(realpath output):/cc/output corpuscleaner-singularity.sif cc; singularity exec instance://cc bash -c \"ray start --address=${master}:6379\"" &
   ((i++))
 done
-
+sleep 30
 singularity exec instance://cc bash -c "cd /cc/corpus-cleaner && RAY_ADDRESS=auto python3.6 clean.py ${PARAMETERS}"
 
 
