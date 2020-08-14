be0fabe9adce1b2ddbfc21fa3ddf6432844ba630
Jordi Armengol Estapé <me@jordiae.com>
2020-08-14 14:20:49 +0200

    Create run-singuarity-lsf.sh

---
+++ run-singuarity-lsf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#BSUB -J   corpuscleaner
+#BSUB -W   48:00
+#BSUB -cwd "."
+#BSUB -oo  "logs/corpuscleaner_%J.out"
+#BSUB -eo  "logs/corpuscleaner_%J.err"
+#BSUB -n   1
+#BSUB -R   "span[ptile=16]"
+#BSUB -x
+
+module purge && module load singularity/3.2.0
+
+PARAMETERS="example-output --input-path data/toy_wiki --input-format wikipedia --output-format fairseq-lm --lang-filter ca"
+
+bash run-singularity.sh ${PARAMETERS}
