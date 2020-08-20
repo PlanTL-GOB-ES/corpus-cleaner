642acfee22cb70aa98ef5341fa215ac69a7ad584
Jordi Armengol Estapé <me@jordiae.com>
2020-08-20 14:20:47 +0200

    Update deploy-bsc.sh

--- deploy-bsc.sh
+++ deploy-bsc.sh
@@ -5,6 +5,6 @@ GROUP="dummygroup"
 VERSION=$(date +"%d-%m-%Y")
 DIR_PATH="/gpfs/projects/${GROUP}/corpus-cleaner/${VERSION}"
 ssh ${USER}@dt01.bsc.es 'mkdir -p ' ${DIR_PATH}/data ${DIR_PATH}/output ${DIR_PATH}/logs ${DIR_PATH}/scripts
-scp -r corpuscleaner-singularity.sif run-singularity.sh run-singularity-slurm.sh run-singularity-slurm-dist.sh ${USER}@dt01.bsc.es:${DIR_PATH}
+scp -r corpuscleaner-singularity.sif run-singularity.sh run-singularity-slurm.sh run-singularity-slurm-dist.sh corpuscleaner-singularity-legacy.sif run-singularity-lsf.sh run-singularity-lsf-dist.sh ${USER}@dt01.bsc.es:${DIR_PATH}
 ssh ${USER}@dt01.bsc.es 'find ' ${DIR_PATH} ' -type d -exec chmod 775 {} +'
 ssh ${USER}@dt01.bsc.es 'find ' ${DIR_PATH} ' -type f -exec chmod 664 {} +'
