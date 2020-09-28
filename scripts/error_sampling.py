fa1ba60c138a3170b3a538233986e415a66262d7
casimiro pio carrino <c.casimiro88@gmail.com>
2020-09-28 11:21:51 +0200

    implemented sentence error sampler based on pipeline"s debug mode

---
+++ scripts/error_sampling.py
@@ -0,0 +1,84 @@
+import argparse
+import random
+import logging
+import os
+
+SCRIPT_DIR = os.path.realpath(__file__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+# 1. Classify sentences in two classes ERROR/NO ERROR
+# 2. Balanced sampling from each class
+
+class ErrorSampler:
+    def __init__(self, sample_size: int, sampling_seed: int):
+        self.sample_size = sample_size
+        self.sampling_seed = sampling_seed
+        self.sentences_errors = {'error': [], 'no_error': []}
+
+    @staticmethod
+    def check_error(sentence_orig, sentence_clean):
+        if sentence_orig.strip() == sentence_clean.strip():
+            return False
+        return True
+
+    # 2. Classify sentences in two classes ERROR/NO ERROR
+    def classify(self, input_file):
+        with open(input_file) as fn:
+            lines = fn.readlines()
+        assert all(len(line.split('\t')) == 2 for line in lines),\
+            "Please input a file in a TAB-separated format obtained from the pipeline executed in debug mode"
+
+        logging.info(f"Classifying sentences in error type from the file: {input_file}")
+        for line in lines:
+            sent_orig, sent_clean = line.split('\t')
+            if self.check_error(sent_orig, sent_clean):
+                self.sentences_errors['error'].append(sent_orig)
+            else:
+                self.sentences_errors['no_error'].append(sent_orig)
+
+    def sample(self, output_file):
+        random.seed(self.sampling_seed)  # set random seed
+
+        # Define a balanced sampling for the ERROR and NO_ERROR classes
+        logging.info("Trying balanced sampling from the 'error' and 'no_error' class")
+        sentences_per_error_type = round(self.sample_size / 2)
+        sample_sentences = []
+        for error, sentences in self.sentences_errors.items():
+            # Check if the number of sampled sentences is greater than the total number of sentences for each
+            # error type before to apply random sampling. If not, select all the sentences of the group type
+            if sentences_per_error_type >= len(sentences):
+                random_sentences = sentences
+                logging.info(f"Random sampling of {len(sentences)} sentences from '{error}' class")
+            else:
+                random_sentences = list(set(random.choices(sentences, k=sentences_per_error_type)))
+                logging.info(f"Random sampling of {sentences_per_error_type} sentences for error type {error}")
+
+            sample_sentences.extend(random_sentences)
+
+        sample_sentences = list(set(sample_sentences))
+
+        with open(output_file, 'w') as of:
+            of.write('\n'.join(sample_sentences))
+        logging.info(f"Collected sample with {len(sample_sentences)} sentences into file {output_file}")
+
+        return sample_sentences
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sample-size', type=int,
+                        help='Number of sentences in the final sample')
+    parser.add_argument('--sampling-seed', type=int, default=54,
+                        help='Seed used for the random sampling')
+    parser.add_argument('--input-file', type=str,
+                        help='File containing the sentences that are sampled by error type')
+    parser.add_argument('--output-file', type=str,
+                        help='File to write the sampled sentences')
+
+    args = parser.parse_args()
+
+    sampler = ErrorSampler(sample_size=args.sample_size, sampling_seed=args.sampling_seed)
+    sampler.classify(args.input_file)
+    sampler.sample(args.output_file)
