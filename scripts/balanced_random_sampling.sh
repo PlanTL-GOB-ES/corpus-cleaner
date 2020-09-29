505e33d481d1d038ef4ec0279afa66b4d382ee3e
casimiro pio carrino <c.casimiro88@gmail.com>
2020-09-29 11:23:42 +0200

    add pipe command to find and shuffle huge list of files + add argument to check the minimum number of lines per file

--- scripts/balanced_random_sampling.sh
+++ scripts/balanced_random_sampling.sh
@@ -7,6 +7,7 @@ exclude_names=${exclude_names}
 sample_size=${sample_size}
 number_files=${number_files}
 output_file=${output_file}
+check_min_number_lines=${check_min_number_lines:-false}
 seed=${seed:-42}
 
 # add named arguments from: https://brianchildress.co/named-parameters-in-bash/
@@ -81,29 +82,33 @@ if [[ -z "${output_file}" ]]; then
   output_file=${SCRIPT_DIR}/sample.${sample_size}.txt
 fi
 
-
-
 total_number_files=$(find_files "${data_dirs}" "${exclude_names}" | wc -l)
 if [[ -z "${number_files}" ]]; then
   number_files=${total_number_files}
 fi
 
 # 1: select the files to extract from
-files=$(find_files "${data_dirs}" "${exclude_names}")
-files_sample=$(random_files_sample "${files}" ${number_files} ${seed})
+# The pipe command is necessary to find and shuffle in the case of huge number of files
+files_sample=$(find_files "${data_dirs}" "${exclude_names}" | \
+  shuf -n ${number_files} --random-source=<(get_seeded_random ${seed}))
+#files_sample=$(random_files_sample "${files}" ${number_files} ${seed})
 number_files_sample=$(echo ${files_sample} | wc -w)
 echo -e "Selected ${number_files_sample} files:\n${files_sample}"
 
-# 2: count the total number of lines, the minimum number of lines and the number of lines to extract per files
+# 2: count the number of lines to extract per file
 total_lines=$(cat ${files_sample} | sed '/^$/d' | wc -l)
-min_lines_number=$(wc -l ${files_sample} | grep -oP "^\s*[0-9]*" | sort | head -n 1)
 number_lines_sample=$(echo "${sample_size}/${number_files_sample}" | bc )
 echo "number lines sample: ${number_lines_sample}"
-echo "minimum number of lines: ${min_lines_number}"
-if [[ ${number_lines_sample} -gt ${min_lines_number} ]]; then
-   echo "Number of samples lines per files greater than minimum number of lines available." \
-        "Set sample_size argument to smaller value"
-   exit 0
+
+# Optionally, check if all files have the minimum number of lines
+if [[ ${check_min_number_lines} == "true" ]]; then
+    min_lines_number=$(wc -l ${files_sample} | grep -oP "^\s*[0-9]*" | sort | head -n 1)
+    echo "minimum number of lines: ${min_lines_number}"
+    if [[ ${number_lines_sample} -gt ${min_lines_number} ]]; then
+       echo "Number of samples lines per files greater than minimum number of lines available." \
+            "Set sample_size argument to smaller value"
+       exit 0
+    fi
 fi
 echo "Total number of lines: ${total_lines}"
 
@@ -112,7 +117,7 @@ echo "Balanced sampling..."
 echo -n > ${output_file}
 
 for file in ${files_sample}; do
-    echo "Sampling lines per file: ${file} (${number_lines_sample})"
+    echo "Attempting to sample ${number_lines_sample} lines from file file: ${file} ($(count_file_lines ${file}) lines)"
     random_lines_sample ${file} ${sample_size} ${number_lines_sample} ${seed} >> ${output_file}
 done
 
