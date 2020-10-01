e806a375ffe46c0a789b8c323bdaee6135a4295f
casimiro pio carrino <c.casimiro88@gmail.com>
2020-10-01 13:45:56 +0200

    add log file to balanced random sampling

--- scripts/balanced_random_sampling.sh
+++ scripts/balanced_random_sampling.sh
@@ -97,31 +97,29 @@ if [[ -z "${output_file}" ]]; then
   fi
 fi
 
-#total_number_files=$(find_files "${data_dirs}" "${exclude_names}" | wc -l)
-#if [[ -z "${number_files}" ]]; then
-#  number_files=${total_number_files}
-#fi
+datetime=$(date '+%d-%m-%Y_%H-%M-%S');
+log_file=${SCRIPT_DIR}/sample_${datetime}.log
 
 # 1: select the files to extract from
 number_dirs=$(echo ${data_dirs} | wc -w)
 number_files_dir=$(echo "${number_files}/${number_dirs}" | bc )
 # The pipe command is necessary to find and shuffle in the case of huge number of files
 for data_dir in ${data_dirs}; do
-    echo "Attempting to sample ${number_files_dir} files from directory: ${data_dir} ($(find_files "${data_dir}" "${exclude_names}" | wc -l)) files"
+    echo "Attempting to sample ${number_files_dir} files from directory: ${data_dir} ($(find_files "${data_dir}" "${exclude_names}" | wc -l)) files" | tee -a ${log_file}
     files_sample+=" "
     files_sample+=$(find_files "${data_dir}" "${exclude_names}" | \
         shuf -n ${number_files_dir} --random-source=<(get_seeded_random ${seed}))
 done
 number_files_sample=$(echo ${files_sample} | wc -w)
 total_lines=$(cat ${files_sample} | sed '/^$/d' | wc -l)
-echo -e "Selected ${number_files_sample} files with total number of lines: ${total_lines}"
+echo -e "Selected ${number_files_sample} files with total number of lines: ${total_lines}" | tee -a ${log_file}
 
 # 2: Sample number_lines_sample from each files
 number_lines_sample=$(echo "${sample_size}/${number_files_sample}" | bc )
 # Optionally, check if all files have the minimum number of lines
 if [[ ${check_min_number_lines} == "true" ]]; then
     min_lines_number=$(wc -l ${files_sample} | grep -oP "^\s*[0-9]*" | sort | head -n 1)
-    echo "minimum number of lines: ${min_lines_number}"
+    echo "minimum number of lines: ${min_lines_number}" | tee -a ${log_file}
     if [[ ${number_lines_sample} -gt ${min_lines_number} ]]; then
        echo "Number of samples lines per files greater than minimum number of lines available." \
             "Set sample_size argument to smaller value"
@@ -132,7 +130,7 @@ fi
 echo "Balanced sampling..."
 echo -n > ${output_file}
 for file in ${files_sample}; do
-    echo "Attempting to sample ${number_lines_sample} lines from file: ${file} ($(count_file_lines ${file}) lines)"
+    echo "Attempting to sample ${number_lines_sample} lines from file: ${file} ($(count_file_lines ${file}) lines)" | tee -a ${log_file}
     if [[ ${debug} == "true" ]]; then
         echo -e "\nFILE: ${file}" >> ${output_file}
     fi
@@ -141,8 +139,8 @@ for file in ${files_sample}; do
 done
 
 if [[ ${debug} != "true" ]]; then
-    echo "Removing duplicates from the final sample"
+    echo "Removing duplicates from the final sample" | tee -a ${log_file}
     sort -u ${output_file} -o ${output_file}
 fi
 sample_lines=$(count_file_lines ${output_file})
-echo "Sampled ${sample_lines} total lines into file: $(realpath ${output_file})"
+echo "Sampled ${sample_lines} total lines into file: $(realpath ${output_file})" | tee -a ${log_file}
