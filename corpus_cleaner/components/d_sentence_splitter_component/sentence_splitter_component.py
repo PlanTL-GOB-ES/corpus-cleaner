b0e1fb44051dad83f2fcb9d6e2106b60f50682fc
casimiro pio carrino <c.casimiro88@gmail.com>
2020-10-06 11:54:32 +0200

    do not align when the number of original and cleaned sentences is not equal

--- corpus_cleaner/components/d_sentence_splitter_component/sentence_splitter_component.py
+++ corpus_cleaner/components/d_sentence_splitter_component/sentence_splitter_component.py
@@ -57,7 +57,10 @@ class SentenceSplitterComponent(CleanerComponentMapper):
                     document.operations.append("_sentence_splitter")
 
                 # Return None the original sentences are not aligned to the cleaned sentences
-                if not len(document.sentences) == len(document.sentences_orig):
+                if not len(document.sentences) > len(document.sentences_orig):
+                    document.sentences_orig = [document.content_orig]
+                    document.sentences_orig.extend([''] * (len(document.sentences) - len(document.sentences_orig)))
+                elif len(document.sentences) < len(document.sentences_orig):
                     return None
 
             # add operations for each sentence in the document
