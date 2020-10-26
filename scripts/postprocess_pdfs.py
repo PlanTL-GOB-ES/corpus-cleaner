6e7d6958c73672953644719e6d72dd732f526578
casimiro pio carrino <c.casimiro88@gmail.com>
2020-10-26 17:03:52 +0100

    postprocess pdfs and keep folder structure

--- scripts/postprocess_pdfs.py
+++ scripts/postprocess_pdfs.py
@@ -7,7 +7,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 data_dir = sys.argv[1]
+output_dir = sys.argv[2]
 p = Path(os.path.realpath(data_dir))
+os.makedirs(output_dir, exist_ok=True)
 
 SUFFIX = ".txt"
 
@@ -23,18 +25,25 @@ def postprocess(file):
 
     with open(os.path.join(data_dir, file)) as fn:
         content = fn.read()
+
     content_postproc = pattern_replace_bullets.sub("", content)
     content_postproc = pattern_line_break_1.sub("", content_postproc)
     content_postproc = pattern_line_break_2.sub(" ", content_postproc)
-    with open(os.path.join(data_dir, f"{file}_postproc"), 'w') as gn:
-        gn.write(content_postproc)
 
     lines_raw = len(content.splitlines())
     lines_postproc = len(content_postproc.splitlines())
+
+    dir_tree = os.path.dirname(os.path.relpath(file, data_dir))
+    output_file = os.path.join(output_dir, dir_tree, f"{os.path.basename(file)}")
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, 'w') as gn:
+        gn.write(content_postproc)
+
     logging.info(f"Raw lines: {lines_raw}, Postprocessed lines: {lines_postproc}, "
-                 f"Difference: {lines_raw - lines_postproc}")
+                 f"Difference: {lines_raw - lines_postproc}, Outfile: {output_file}")
 
 
-for file in p.rglob(f"*{SUFFIX}"):
+files = list(p.rglob(f"*{SUFFIX}"))
+for file in files:
     logging.info(f"Processing file {file}")
     postprocess(file)
