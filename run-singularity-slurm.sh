18123b9525590d83622a62bb702a1d3598404db9
Jordi Armengol Estapé <me@jordiae.com>
2020-10-26 13:11:41 +0100

    Update run-singularity-slurm.sh

--- run-singularity-slurm.sh
+++ run-singularity-slurm.sh
@@ -9,6 +9,7 @@
 
 module load singularity/3.5.2
 
+# Use parallel to run in parallel (1 node)
 PARAMETERS="example-output --input-path data/toy_wiki --input-format wikipedia --output-format fairseq-lm --lang-filter ca"
 
 bash run-singularity.sh ${PARAMETERS}
