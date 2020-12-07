15f7c16f158760815bb5a0cfd987bf8d6625d738
Jordi Armengol Estapé <me@jordiae.com>
2020-12-07 15:22:14 +0100

    Update filter_no_letter_lines_and_short_single_sent_docs.sh

--- scripts/filter_no_letter_lines_and_short_single_sent_docs.sh
+++ scripts/filter_no_letter_lines_and_short_single_sent_docs.sh
@@ -2,7 +2,7 @@
 
 "Filter sentences without any ASCII letter, and single-sentence documents with less than 5 tokens"
 
-INPUT=colossal_ca_v3_20201203.txt
-OUTPUT=colossal_ca_v3_20201207.txt
+INPUT=corpus.txt
+OUTPUT=corpus2.txt
 
 sed -E -e '/[a-zA-Z]|^\s*$/!d' $INPUT |  awk -vRS= 'NF<5 && $0 !~ /\n/ {next} {print; print "\n"}' > $OUTPUT
