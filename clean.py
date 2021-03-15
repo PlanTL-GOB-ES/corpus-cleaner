f1dbf4d0b54c4f83a335d9c1f4373474d36db0ab
Casimiro Pio Carrino <c.casimiro88@gmail.com>
2021-03-15 12:50:53 +0100

    add config to clean.py

--- clean.py
+++ clean.py
@@ -10,12 +10,10 @@ from corpus_cleaner.cleaner import CleanerConfig
 from simple_parsing import ArgumentParser
 
 
-# TODO: initialize simpleparsing: https://github.com/lebrice/SimpleParsing
-
-def clean(args: argparse.Namespace, logger: logging.Logger, checkpoint: Checkpoint):
-    logger.info(args)
+def clean(config: CleanerConfig, logger: logging.Logger, checkpoint: Checkpoint):
+    logger.info(config)
     t0 = datetime.datetime.now().timestamp()
-    cleaner = Cleaner(args, logger, checkpoint)
+    cleaner = Cleaner(config, logger, checkpoint)
     cleaner.clean()
     checkpoint.declare_as_cleaned()
     t1 = datetime.datetime.now().timestamp()
@@ -28,13 +26,14 @@ def get_output_dir(name: str, output_path: str) -> str:
     return output_dir
 
 
+# TODO: check args with __post_init_() method in each Config class
 def check_args(args: argparse.Namespace):
     for path in [args.input_path, args.output_path]:
         if path is None or not os.path.exists(path):
             raise FileNotFoundError(path)
         if not os.path.isdir(args.input_path):
             raise NotADirectoryError(path)
-    (valid_input_formats, valid_output_formats) = Cleaner.get_valid_input_output_formats()
+        (valid_input_formats, valid_output_formats) = Cleaner.get_valid_input_output_formats()
     if args.input_path == args.output_path:
         raise Exception('Input and output paths should be different')
     if args.input_format not in valid_input_formats:
@@ -49,34 +48,26 @@ def check_args(args: argparse.Namespace):
 def main():
     parser = ArgumentParser(description='Clean raw text data.')
 
-    parser.add_argument(CleanerConfig, dest='cleaner_config')
-    # parser.add_argument('name', type=str, help='A name to identify the run')
-    # parser.add_argument('--input-path', type=str, help='Input data directory')
-    # parser.add_argument('--output-path', type=str, help='Output data directory', default='output')
-    # parser.add_argument('--input-format', type=str, help='Input data format')
-    # parser.add_argument('--output-format', type=str, help='Output data format')
-    # parser.add_argument('--checkpoint-backend', choices=['shelve', 'file'], default='shelve',
-    #                     help='Shelve is more convenient but file is more robust. For distributed executions,'
-    #                          'we recommend file.')
+    parser.add_argument(CleanerConfig, dest='config')
 
     args = parser.parse_args()
 
-    check_args(args)
+    # check_args(args)
 
     args.corpus_cleaner_version = corpus_cleaner.__version__
-    output_dir = get_output_dir(args.name, args.output_path)
+    output_dir = get_output_dir(args.config.global_config.name, args.config.global_config.output_path)
     args.output_path = output_dir
 
     if os.path.exists(output_dir):
         raise OSError(f'{output_dir} already exists!')
     os.makedirs(output_dir, exist_ok=True)
 
-    checkpoint = Checkpoint(output_dir, args)
+    checkpoint = Checkpoint(args.config.global_config.output_path, args)
     logger = checkpoint.logger
 
     logging.info(output_dir)
 
-    clean(args, logger, checkpoint)
+    clean(args.config, logger, checkpoint)
 
     print()
 
