befcc227cc08e7b1eb0934ec33eda20f8b779f83
Casimiro Pio Carrino <c.casimiro88@gmail.com>
2021-03-29 17:01:55 +0200

    add script to fix "broken sentences" using regex expressions

--- scripts/fix_sentence_breaks.py
+++ scripts/fix_sentence_breaks.py
@@ -3,25 +3,35 @@ import sys
 import re
 import os
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO)
 
 
-def postprocess(file):
+def postprocess(file, data_dir, output_dir):
+    # remove space before new lines
+    pattern_replace_trailing_spaces = re.compile(r" *\n")
+
     # remove bullets point starting with L
     pattern_replace_bullets = re.compile(r" *ʟ *")
 
     # recover hyphen-splitter sentences
     pattern_line_break_1 = re.compile(r"-\n")
+
     # recover sentences split not preceded by a period and followed by lowercase character
-    pattern_line_break_2 = re.compile(r"(?<![.?¿!¡])\s*\n(?=\s*[a-z])")
+    pattern_line_break_2 = re.compile(r"(?<![.?¿!¡])\n(?=\s*[a-z])")
+
+    # recover sentences split by preceded by numerical bullet points
+    pattern_line_break_3 = re.compile(r"(?<=[0-9]\.)\n")
 
     with open(os.path.join(data_dir, file)) as fn:
         content = fn.read()
 
-    content_postproc = pattern_replace_bullets.sub("", content)
+    content_postproc = pattern_replace_trailing_spaces.sub('\n', content)
+    content_postproc = pattern_replace_bullets.sub("", content_postproc)
     content_postproc = pattern_line_break_1.sub("", content_postproc)
-    content_postproc = pattern_line_break_2.sub(" ", content_postproc)
+    content_postproc = pattern_line_break_2.sub("", content_postproc)
+    content_postproc = pattern_line_break_3.sub(" ", content_postproc)
 
     lines_raw = len(content.splitlines())
     lines_postproc = len(content_postproc.splitlines())
@@ -37,14 +47,17 @@ def postprocess(file):
 
 
 if __name__ == '__main__':
-    data_dir = sys.argv[1]
-    output_dir = sys.argv[2]
-    p = Path(os.path.realpath(data_dir))
-    os.makedirs(output_dir, exist_ok=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, help='directory with data (processed recursively in case of subdirs)')
+    parser.add_argument('--output_dir', type=str, help='output directory with processed data')
+    args = parser.parse_args()
+
+    p = Path(os.path.realpath(args.data_dir))
+    os.makedirs(args.output_dir, exist_ok=True)
 
     SUFFIX = ".txt"
 
     files = list(p.rglob(f"*{SUFFIX}"))
     for file in files:
         logging.info(f"Processing file {file}")
-        postprocess(file)
+        postprocess(file, args.data_dir, args.output_dir)
