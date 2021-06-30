0bcc0756b5c1730ec833a947e21fc45f70428c09
onadegibert <onadegibert@gmail.com>
2021-06-30 18:33:44 +0200

    Update oc.py missing parentheses

--- corpus_cleaner/configs/langs/oc.py
+++ corpus_cleaner/configs/langs/oc.py
@@ -1,7 +1,7 @@
 
 import string
 
-alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase)
+alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase))
 fasttext = True
 langid = True
 
