aacea4d8fcf7ece6ebf6c9d3644fff3a96e8d8fa
onadegibert <onadegibert@gmail.com>
2021-06-30 17:35:05 +0200

    Corrected occitan file

--- corpus_cleaner/configs/langs/oc.py
+++ corpus_cleaner/configs/langs/oc.py
@@ -5,4 +5,4 @@ alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase)
 fasttext = True
 langid = True
 
-ca = dict(alphabet=alphabet, fasttext=fasttext, langid=langid)
+oc = dict(alphabet=alphabet, fasttext=fasttext, langid=langid)
