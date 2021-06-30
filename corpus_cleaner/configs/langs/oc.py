52af41edb6e9e66ae9cd9800d65fe8b32f8a1faf
onadegibert <onadegibert@gmail.com>
2021-06-30 17:34:22 +0200

    Added support for occitan

---
+++ corpus_cleaner/configs/langs/oc.py
@@ -0,0 +1,8 @@
+
+import string
+
+alphabet = set(list(string.ascii_uppercase) + list(string.ascii_lowercase)
+fasttext = True
+langid = True
+
+ca = dict(alphabet=alphabet, fasttext=fasttext, langid=langid)
