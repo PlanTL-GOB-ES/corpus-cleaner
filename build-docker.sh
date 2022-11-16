8cf9dbec65bf26702bc96b8dc501bbff6e13f1cc
Casimiro Pio Carrino <c.casimiro88@gmail.com>
2022-11-16 14:03:17 +0100

    create release note in Docker build script

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 branch=$1
 
+# First, create release note for the current version
+RELEASE_NOTE="release-note-container.txt"
+echo "Branch: ${branch}" > ${RELEASE_NOTE}
+start=$(git log master..${branch} | grep -n commit | sed -n "1p" | cut -d ":" -f 1)
+end=$(git log | grep -n commit | sed -n "2p" | cut -d ":" -f 1)
+end=$(echo ${end} - 1 | bc)
+echo $(git log | sed -n "${start},${end}p") >> ${RELEASE_NOTE}
+
 docker build  -t corpuscleaner --build-arg BRANCH=$branch .
--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,6 @@ DEPLOY_DIR=$3
 NAME=$4
 
 VERSION=$(date +"%d-%m-%Y")-${NAME}
-
-# First, create release note for the current version
-RELEASE_NOTE="release-note-container.txt"
-start=$(git log | grep -n commit | sed -n "1p" | cut -d ":" -f 1)
-end=$(git log | grep -n commit | sed -n "2p" | cut -d ":" -f 1)
-end=$(echo ${end} - 1 | bc)
-
-echo $(git log | sed -n "${start},${end}p") > ${RELEASE_NOTE}
 echo -e "\nPlease, add a brief description of the deploy below:" >> ${RELEASE_NOTE}
 vim ${RELEASE_NOTE}
 
